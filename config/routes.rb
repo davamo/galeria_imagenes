d8db799aeac1d5df6cd688d41a9b1348aeb520d5
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-05-20 23:39:06 -0400

    Feat: refactor applicartion.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'pages/index'
-  #get 'images/thumbs'
+  #get 'pages/webfonts'
   #get 'images/fulls'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
