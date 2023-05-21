e52c7056ce16684da31ff61b4ae379e90615df6c
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-05-20 22:39:18 -0400

    Feat: modified path images

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+  #get 'images/thumbs'
+  #get 'images/fulls'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
 end
