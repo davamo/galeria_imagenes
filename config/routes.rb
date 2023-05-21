94fffec399dfcbf2cfcc936212a06196f053bf8a
davamo <8826599-davamo@users.noreply.gitlab.com>
2023-05-21 15:37:19 -0400

    Fixedt: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'pages' , to: 'pages#index'
   get 'information', to: 'information#info', as: "information"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root "pages#index"
 end
